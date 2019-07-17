70d70dd5083c89b4fb83e4b0abd04d0c474412b9
lu lee <lulee@ludeMac-mini.lan>
2019-07-18 01:26:14 +0800

    学习变量类型，根据索引获取数据

--- code/main.py
+++ code/main.py
@@ -1,1 +1,19 @@
-print9('hello world')
+a = 'ABCDE'
+print(a)
+print(a[2])
+print(a[3:])
+b = a[-1]
+print(b)
+b = 8
+print(b)
+print(1+b)
+c = 12
+print(c+b)
+print(c*b)
+g = 98
+print(g+c)
+print(c+g)
+print(g/c)
+print(g/b)
+
+
